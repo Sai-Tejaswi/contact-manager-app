e1083570903b8d396994f5a8fe61aa7adb39be67
Sai-Tejaswi <chisate1998@gmail.com>
2021-06-04 18:17:13 +0530

    created contact manager site by adding mails and stored locally

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,43 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-import React from 'react';
-import Header from './Header';
-import AddContact from './AddContact';
-import ContactList from './ContactList';
+import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
 
 function App() {
-    const contacts = [
-        {
-            id:"1",
-            name:"Deepu",
-            email:"deepika@gmail.com",
-        },
-        {
-            id:"2",
-            name:"vana",
-            email:"vanaja@gmail.com",
-        },
-    ];
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
 
-    return (
-        <div className="ui container">
-            <Header />
-            <AddContact/>
-            <ContactList contacts ={contacts}/>
-        </div>
-    );
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, {  ...contact }]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  };
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
+    </div>
+  );
 }
 
-export default App
+export default App;
